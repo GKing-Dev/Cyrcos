99d5288772df65fd26904621e73e0dee2b5dc1fc
Greg King <greg@king-dev.com>
2017-11-02 11:06:10 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name = "Cyrcos",
+	description = "A Circos-like implementation in Python 3 using Matplotlib",
+	version = "1.0",
+	author = "Greg King",
+	author_email = "greg@king-dev.com",
+	packages = ["Cyrcos"],
+	install_requires = ["matplotlib"],
+	licence = "MIT",
+	url = "https://github.com/GKing-Dev/Cyrcos",
+	keywords = "Circos matplotlib chord graph visualization graphics"
+)
